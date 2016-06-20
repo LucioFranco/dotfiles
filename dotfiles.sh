9185128e12f7237b53a87730705727de0d04efbd
Lucio Franco <lucio.franco@du.edu>
2016-06-20 09:32:52 -0600

    updated dotfiles script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-LIST=".i3 .i3status.conf .spacemacs .bash_aliases"
+LIST=".i3/config .i3status.conf .spacemacs .bash_aliases"
 
-if [ "$1" != "cp" ]
+if [ "$1" == "up" ]
 then
     echo "putting files into ~/"
     for I in $LIST; do
         echo "Copying file $I"
 	      cp -R $I ~/$I
     done
-else
+elif [ "$1" == "down"  ]
+then
     echo "Copying files from ~/"
     for I in  $LIST; do
         echo "Copying file $I"
         cp -R ~/$I $I
     done
+else
+    echo "Usage: ./dotfiles [command]"
+    echo "Commands:"
+    echo "    up - will put files into ~/"
+    echo "    down - will copy files from ~/"
 fi
